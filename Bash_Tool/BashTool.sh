53553bc00de497ad0206648b615cbd4e97f24b3b
ZeroDot1 <blindgrafix@googlemail.com>
2017-10-22 18:35:59 +0200

    Bash Tool Last Update: 22.10.2017 18:36

--- Bash_Tool/BashTool.sh
+++ Bash_Tool/BashTool.sh
@@ -7,7 +7,7 @@ options=(
     1 "Sync Pacman Database"
     2 "Update the System (Pacman)"
     3 "Update the System (Pacaur)"
-    4 "Update-Hosts Ad and Malwareblock" 
+    4 "Update-Hosts Ad and Malwareblock"
     5 "Install Update-Hosts Ad and Malwareblock"
     6 "Clean pacman cache completely and remove all packages"
     7 "Optimize the pacman database access speed"
@@ -22,7 +22,9 @@ options=(
     16 "Rankmirrors to Set the Fastest Download Server"
     17 "Initializing the keyring and Verifying the master keys"
     18 "Refresh mirror list (reflector)"
-    19 "Exit :("
+    19 "Turn off the computer"
+    20 "Restart the computer"
+    21 "Exit :("
     )
 
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
@@ -34,43 +36,43 @@ do
             clear
             tput setaf 2
             echo "Sync Pacman Database"
-            sudo pacman -Syy --force 
+            sudo pacman -Syy --force
             echo "Done!" ;;
         2)
             clear
             tput setaf 2
             echo "Update the Syststem (Pacman)"
-            sudo pacman -Syyu --force 
+            sudo pacman -Syyu --force
             echo "Done!" ;;
         3)
             clear
             tput setaf 2
             echo "Update the Syststem (Pacaur)"
-            pacaur -Syyu --force 
+            pacaur -Syyu --force
             echo "Done!" ;;
         4)
             clear
             tput setaf 2
             echo "Update-Hosts Ad and Malwareblock"
-            update-hosts 
+            update-hosts
             echo "Done!" ;;
         5)
             clear
             tput setaf 2
             echo "Install Update-Hosts Ad and Malwareblock"
-            pacaur -Syy --force update-hosts 
+            pacaur -Syy --force update-hosts
             echo "Done!" ;;
         6)
             clear
             tput setaf 2
             echo "Clean pacman cache completely and remove all packages"
-            sudo pacman -Scc 
+            sudo pacman -Scc
             echo "Done!" ;;
         7)
             clear
             tput setaf 2
             echo "Optimize the pacman database access speed"
-            sudo pacman-optimize && sync 
+            sudo pacman-optimize && sync
             echo "Done!" ;;
         8)
             clear
@@ -90,8 +92,8 @@ do
         11)
             clear
             echo "About Bash Tool"
-            dialog --backtitle "Bash Tool, Bash Quick and easy! by ZeroDot1" --title 'About Bash Tool' --msgbox ' Bash Tool, Bash Quick and easy! \n Version 1.0.0.2 \n (c) 2017 by ZeroDot1 \n 
- Homepage: https://galaxy-bytes.blogspot.de/ \n Github: https://github.com/ZeroDot1 \n \n Last Update: 19.10.2017 14:52' 15 60
+            dialog --backtitle "Bash Tool, Bash Quick and easy! by ZeroDot1" --title 'About Bash Tool' --msgbox ' Bash Tool, Bash Quick and easy! \n Version 1.0.0.3 \n (c) 2017 by ZeroDot1 \n
+ Homepage: https://galaxy-bytes.blogspot.de/ \n Github: https://github.com/ZeroDot1 \n \n Last Update: 22.10.2017 18:36' 15 60
             clear ;;
         12)
             clear
@@ -99,13 +101,13 @@ do
         13)
             clear
             echo "NVIDIA Automatic configuration (nvidia-xconfig)"
-            sudo nvidia-xconfig 
+            sudo nvidia-xconfig
             echo "Done!" ;;
         14)
             clear
             echo "ClamAV update the virus definitions"
             sudo freshclam -d
-            sudo freshclam 
+            sudo freshclam
             echo "Done!" ;;
         15)
             clear
@@ -117,7 +119,7 @@ do
             cd /etc/pacman.d/
             sudo cp mirrorlist mirrorlist.bak
             rankmirrors -n 6 mirrorlist.bak | sudo tee mirrorlist
-            sudo pacman -Syy --force 
+            sudo pacman -Syy --force
             echo "Done!" ;;
         17)
             clear
@@ -126,7 +128,7 @@ do
             sudo pacman-key --init
             sudo pacman-key --populate archlinux antergos
             sudo pacman-key --refresh-keys
-            sudo pacman -Syy --force 
+            sudo pacman -Syy --force
             cd $HOME
             echo "Done!" ;;
         18)
@@ -136,6 +138,16 @@ do
             clear
             echo "Done!" ;;
         19)
+            clear
+            echo "Turn off the computer"
+            poweroff
+            clear ;;
+        20)
+            clear
+            echo "Restart the computer"
+            reboot
+            clear ;;
+        21)
             clear
             echo "Exit :("
             exit 0
